06e93ab9d88af75e573fe24d324541acdcea112b
h.iijima <easy.s2m@gmail.com>
2024-10-23 11:33:34 +0900

    BrowserRouterとRoute をインポート

--- task-manager-frontend/src/App.js
+++ task-manager-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import useCategories from "./hooks/useCategories";
 import UserForm from "./components/UserForm";
