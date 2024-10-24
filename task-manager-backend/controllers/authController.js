c0d241b67d0c9d0d34badfbe2eb12ca2f0af7b18
h.iijima <easy.s2m@gmail.com>
2024-10-24 18:58:45 +0900

    ユーザー登録機能の修正

--- task-manager-backend/controllers/authController.js
+++ task-manager-backend/controllers/authController.js
@@ -1,4 +1,6 @@
 const db = require("../config/database");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // 全ユーザーを取得する
 const getUsers = (req, res) => {
@@ -6,57 +8,33 @@ const getUsers = (req, res) => {
     db.query(sqlSelect, (err, result) => {
         if (err) {
             console.error(err);
-            res.status(500).send("Error retrieving users from the database");
-        } else {
-            res.send(result);
+            return res.status(500).send("Error retrieving users from the database");
         }
+        res.send(result);
     });
 };
 
 // 新しいユーザーを追加する
-const insertUser = (req, res) => {
+const insertUser = async (req, res) => {
     const { userName, password } = req.body;
-    const sqlInsert = "INSERT INTO users (userName, password) VALUES (?, ?)";
-    db.query(sqlInsert, [userName, password], (err, result) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send("Failed to insert new user");
-        } else {
-            res.status(201).send("User added successfully");
-        }
-    });
+    if (!userName || !password) {
+        return res.status(400).send("ユーザー名とパスワードを入力してください。");
+    }
+
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const sqlInsert = "INSERT INTO users (userName, password) VALUES (?, ?)";
+      db.query(sqlInsert, [userName, hashedPassword], (err, result) => {
+          if (err) {
+              console.error(err);
+              return res.status(500).send("Failed to insert new user");
+          }
+          return res.status(201).send("User added successfully");
+      });
+  } catch (error) {
+      console.error(error);
+      return res.status(500).send("Error hashing password");
+  }
 };
 
-// ユーザーを更新する
-const updateUser = (req, res) => {
-  const { id, userName, password } = req.body;
-  const sqlUpdate = "UPDATE users SET userName = ?, password = ? WHERE id = ?";
-  db.query(sqlUpdate, [id, userName, password], (err, result) => {
-      if (err) {
-          console.error(err);
-          res.status(500).send("Failed to update Uuer");
-      } else if (result.affectedRows === 0) {
-          res.status(404).send("User not found");
-      } else {
-          res.send({ id, userName, password });
-      }
-  });
-};
-
-// ユーザーを削除する
-// const deleteUser = (req, res) => {
-//   const { id } = req.body;
-//   const sqlDelete = "DELETE FROM users WHERE id = ?";
-//   db.query(sqlDelete, [id], (err, result) => {
-//       if (err) {
-//           console.error(err);
-//           res.status(500).send("Failed to delete user");
-//       } else if (result.affectedRows === 0) {
-//           res.status(404).send("User not found");
-//       } else {
-//           res.send({ message: "User deleted successfully" });
-//       }
-//   });
-// };
-
-module.exports = { getUsers, insertUser, updateUser, /*deleteUser*/ };
+module.exports = { getUsers, insertUser };
