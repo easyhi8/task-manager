875804572dff8f7046d8c2343141bf8468c17c2f
h.iijima <easy.s2m@gmail.com>
2024-10-24 10:55:03 +0900

    タスク追加フォームの修正

--- task-manager-frontend/src/components/TaskForm.js
+++ task-manager-frontend/src/components/TaskForm.js
@@ -8,8 +8,8 @@ const TaskForm = ({ addTask }) => {
     <div>
         <p>タスクの新規追加</p>
         <div className="textBox">
-            <input type="text" placeholder="Name" value={title} onChange={(e) => setTitle(e.target.value)} /><br />
-            <input type="text" placeholder="Email" value={description} onChange={(e) => setDescription(e.target.value)} /><br />
+            <input type="text" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} /><br />
+            <input type="text" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} /><br />
             <button onClick={() => addTask(title, description)}>追加</button>
         </div>
     </div>
