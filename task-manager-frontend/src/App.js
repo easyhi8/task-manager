7c4738eae006568ede83518a976a619607b0f7ff
h.iijima <easy.s2m@gmail.com>
2024-10-25 15:39:56 +0900

    リロード時にログインページが開く処理を追加

--- task-manager-frontend/src/App.js
+++ task-manager-frontend/src/App.js
@@ -1,18 +1,22 @@
 //App.js
-import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect }  from "react";
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import AuthForm from "./components/AuthForm"
 import TaskPage from "./components/TaskForm";
 
 function App() {
+  const navigate = useNavigate();
+    useEffect(() => {
+        navigate("/");
+    }, [navigate]);
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<AuthForm/>} />
-        <Route path="/tasks" element={<TaskPage/>} />
-      </Routes>
+        <Routes>
+            <Route path="/" element={<AuthForm />} />
+            <Route path="/tasks" element={<TaskPage />} />
+        </Routes>
     </div>
     );
 }
