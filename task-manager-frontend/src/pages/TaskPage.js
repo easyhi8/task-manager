06d329971190c7afc5687bbcaaf765d9577d4ddc
h.iijima <easy.s2m@gmail.com>
2024-10-25 10:58:44 +0900

    タスク管理ページの作成

---
+++ task-manager-frontend/src/pages/TaskPage.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import TaskForm from "./components/TaskForm";
+import taskService from "./services/taskService";
+import TaskList from "./components/TaskList";
+
+function TaskManagement() {
+
+  const addTask = (title, description) => {
+      taskService.addTask(title, description)
+          .then(() => {
+              alert("Task added successfully");
+          })
+          .catch(err => {
+              console.error("Error adding task: ", err);
+              alert("Failed to add task");
+          });
+  };
+
+return (
+  <div className="TaskManagement">
+        <div>
+          <h1>タスク管理ページ</h1>
+          <TaskList />
+          <TaskForm addTask={addTask} />
+        </div>
+  </div>
+  );
+}
+
+export default TaskManagement;
