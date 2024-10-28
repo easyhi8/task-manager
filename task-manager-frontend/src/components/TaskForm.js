af26e687b375ed8ce8ba28f5713064d3c2c5f077
h.iijima <easy.s2m@gmail.com>
2024-10-28 10:47:52 +0900

    期日の入力欄を追加

--- task-manager-frontend/src/components/TaskForm.js
+++ task-manager-frontend/src/components/TaskForm.js
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const TaskForm = ({ addTask }) => {
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
+    const [deadline, setDeadline] = useState("");
+    
+    useEffect(() => {
+      const today = new Date().toISOString().split('T')[0]; // YYYY-MM-DD形式で今日の日付を取得
+      setDeadline(today);
+  }, []);
     
     const handleClick = (e) => {
-        if (!title || !description) {
+        if (!title || !description || !deadline) {
               alert("タイトルと説明を入力してください。");
               return;
           }
@@ -17,7 +23,9 @@ const TaskForm = ({ addTask }) => {
     const handleLogout = () => {
       localStorage.removeItem("token");
       window.location.href = "/";
-  };
+    };
+    
+    
 
   return (
     <div>
@@ -25,6 +33,7 @@ const TaskForm = ({ addTask }) => {
         <div className="textBox">
             <input type="text" placeholder="タスクタイトル" value={title} onChange={(e) => setTitle(e.target.value)} /><br />
             <textarea placeholder="タスクの説明" value={description} onChange={(e) => setDescription(e.target.value)} rows="2" /><br />
+            <input type="date" id="dateInput" value={deadline} onChange={(e) => setDeadline(e.target.value)} /><br />
             <div style={{ display: "flex", justifyContent: "space-between", marginTop: "10px" }}>
               <button onClick={handleClick}>追加</button>
               <button style={{marginRight: "0"}} onClick={handleLogout}>ログアウト</button>
