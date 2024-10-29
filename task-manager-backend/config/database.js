4f0c86e9ce5de1070fe7b91ebba0b45d4db6dcdb
h.iijima <easy.s2m@gmail.com>
2024-10-29 18:45:39 +0900

    database.jsにコメントアウト追加

--- task-manager-backend/config/database.js
+++ task-manager-backend/config/database.js
@@ -1,10 +1,12 @@
+//database.js
 const mysql = require("mysql2");
 
+//MySQLデータベースへの接続プールを作成
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "rootroot",
-    database: "task-manager"
+    host: "localhost",       // データベースサーバーのホスト名
+    user: "root",            // データベースに接続するためのユーザー名
+    password: "rootroot",    // データベースユーザーのパスワード
+    database: "task-manager" // 接続するデータベース名
 });
 
 module.exports = db;
