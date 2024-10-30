489b5352e6eed3eb1ca09e2b4691688b6b536504
h.iijima <easy.s2m@gmail.com>
2024-10-30 10:32:42 +0900

    task.jsにコメントアウト追加

--- task-manager-backend/routes/tasks.js
+++ task-manager-backend/routes/tasks.js
@@ -3,14 +3,19 @@ const express = require("express");
 const router = express.Router();
 const tasksController = require("../controllers/tasksController");
 
-router.get("/tasks", tasksController.getTasks);
+//すべてのタスクを取得するためのGETリクエストのルートを定義
+router.get("/tasks", tasksController.getAllTasks);
 
+//特定のタスクをIDで取得するためのGETリクエストのルートを定義
 router.get("/tasks/:id", tasksController.getTask);
 
+//新しいタスクを追加するためのPOSTリクエストのルートを定義
 router.post("/tasks", tasksController.addTask);
 
+//特定のタスクをIDで更新するためのPUTリクエストのルートを定義
 router.put("/tasks/:id", tasksController.updateTask);
 
+//特定のタスクをIDで削除するためのDELETEリクエストのルートを定義
 router.delete("/tasks/:id", tasksController.deleteTask);
 
 module.exports = router;
