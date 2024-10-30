9ee58e5ed13f1b6231d64f9be23016ed4ae8baef
h.iijima <easy.s2m@gmail.com>
2024-10-30 11:15:08 +0900

    server.jsにコメントアウト追加

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -4,11 +4,15 @@ const app = express();
 const bodyParser = require("body-parser");
 const taskRoutes = require("./routes/task");
 
+//アプリケーションにJSONボディパーサーを追加
 app.use(bodyParser.json());
+//apiパスにタスク関連のルートを使用
 app.use("/api", taskRoutes);
 
+//環境変数からポート番号を取得。指定がなければ3001を使用
 const PORT = process.env.PORT || 3001;
 
+//指定したポートでサーバーをリッスンし、起動メッセージをコンソールに表示
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
