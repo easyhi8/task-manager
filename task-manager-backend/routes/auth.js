8df40b7238fc3f807cc365aa8ca32574d4865905
h.iijima <easy.s2m@gmail.com>
2024-10-30 10:29:24 +0900

    auth.jsにコメントアウト追加

--- task-manager-backend/routes/auth.js
+++ task-manager-backend/routes/auth.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+//ユーザー登録用のエンドポイントを定義
 router.post("/register", authController.insertUser);
 
+//ログイン用のエンドポイントを定義
 router.post("/login", authController.loginUser);
 
 module.exports = router;
