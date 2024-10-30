9521854be98be191095abbe0f89081282e0f28e0
h.iijima <easy.s2m@gmail.com>
2024-10-30 15:14:30 +0900

    index.jsにコメントアウト追加

--- task-manager-frontend/src/index.js
+++ task-manager-frontend/src/index.js
@@ -1,11 +1,13 @@
+//index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+//HTMLのルート要素を取得し、Reactのルートを作成
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter>  {/* アプリ全体をBrowserRouterで包み、ルーティングを有効にする */}
         <App />
     </BrowserRouter>
 );
