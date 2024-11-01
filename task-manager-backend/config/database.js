0c5168c3d93d77e5c0771cf26d3866c2285fe170
h.iijima <easy.s2m@gmail.com>
2024-11-01 12:35:30 +0900

    不要なカンマの削除

--- task-manager-backend/config/database.js
+++ task-manager-backend/config/database.js
@@ -4,7 +4,7 @@ require("dotenv").config(); // 環境変数を読み込む
 
 // MySQLデータベースへの接続プールを作成
 const db = mysql.createPool({
-  host: process.env.DB_HOST,, // データベースサーバーのホスト名
+  host: process.env.DB_HOST, // データベースサーバーのホスト名
   user: process.env.DB_USER, // データベースに接続するためのユーザー名
   password: process.env.DB_PASSWORD, // データベースユーザーのパスワード
   database: proces.env.DB_NAME, // 接続するデータベース名
